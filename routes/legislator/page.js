d0ea8647785175660f98e1ef6677991ded5481e8
jiyeonkim7 <jiyean9629@gmail.com>
2018-07-05 02:10:27 +0900

    finish legislator page without contents about him _jiyeon

--- routes/legislator/page.js
+++ routes/legislator/page.js
@@ -10,70 +10,115 @@ const db = require('../../module/pool.js');
 /*  /legislator/page/:l_id  */
 router.get('/:l_id', async(req, res, next) => {
 
-  let l_id = req.params.u_id;
+  let l_id =+ req.params.l_id;
 
   try {
-    let listSql = "SELECT * FROM legislator LEFT OUTER JOIN "
-        listSql += "(SELECT lv_legislator_id, count(*) as score FROM legislatorVote "
-        listSql += "WHERE islike=? GROUP BY lv_legislator_id) as lv "
-        listSql += "ON legislator.id = lv.lv_legislator_id ORDER BY lv.score DESC;"
+    let rankingSql = "SELECT * FROM legislator LEFT OUTER JOIN "
+        rankingSql += "(SELECT lv_legislator_id, count(*) as score FROM legislatorVote "
+        rankingSql += "WHERE islike=? GROUP BY lv_legislator_id) as lv "
+        rankingSql += "ON legislator.id = lv.lv_legislator_id ORDER BY lv.score DESC;"
 
-    let listQuery = await db.queryParamCnt_Arr(listSql,[islike]);
+    let likeRankingQuery = await db.queryParamCnt_Arr(rankingSql,[1]);
+    let unlikeRankingQuery = await db.queryParamCnt_Arr(rankingSql,[0]);
 
-    if(listQuery.length == 0){
+    if(likeRankingQuery.length == 0 || unlikeRankingQuery.length == 0){
       console.log("query not ok");
     }else{
       console.log("query ok");
     }
 
-    let votedSql = "SELECT * FROM legislatorVote WHERE lv_user_id = ? AND islike = ?;"
-    let votedQuery = await db.queryParamCnt_Arr(votedSql, [u_id, islike]);
-
-    if(listQuery.length == 0){
-      console.log("query not ok");
-    }else{
-      console.log("query ok");
-    }
-
-    var result = [];
-    for(var i=0; i<listQuery.length; i++){
+    // 호감 순 줄 세우기
+    var likeRresult = [];
+    for(var i=0; i<likeRankingQuery.length; i++){
       var rankingInfo = {};
 
-      rankingInfo.l_id = listQuery[i].id;
-      rankingInfo.l_name = listQuery[i].name;
-      rankingInfo.party_name = listQuery[i].l_party_name;
+      rankingInfo.id = likeRankingQuery[i].id;
+      rankingInfo.score = likeRankingQuery[i].score;
 
-      rankingInfo.info = '';
-      if (listQuery[i].isPpresident) { // 당대표
-        rankingInfo.info += "당 대표"
-      }
-      if (listQuery[i].isLpresident) { // 원내대표
-        rankingInfo.info += "원내 대표"
-      }
-      if (listQuery[i].isPPpresident) { // 비례대표
-        if (rankingInfo.info != '')
-          rankingInfo.info += ", "
-        rankingInfo.info += "비례 대표"
-      }
-      if (listQuery[i].region_city != "") {  // 지역구+선거구
-        if (rankingInfo.info != '')
-          rankingInfo.info += ", "
-        rankingInfo.info += listQuery[i].region_city + " ";
-        rankingInfo.info += listQuery[i].region_state;
-      }
-      
-      rankingInfo.score = listQuery[i].score;
-      rankingInfo.profileimg = listQuery[i].profile_img_url;
-
-      for (var j=0; j<votedQuery.length; j++)
-        if (listQuery[i].id == votedQuery[j].lv_legislator_id) {
-          rankingInfo.voted = true;
-          break;
+      likeRresult.push(rankingInfo);
+    }
+    // 호감 순 랭킹 뽑기
+    for(var i=0; i<likeRresult.length; i++) {
+      if (i==0) {
+        likeRresult[i].ranking = 1;
+      } else {
+        if(likeRresult[i].score == likeRresult[i-1].score) {
+          likeRresult[i].ranking = likeRresult[i-1].ranking;
+        } else if (likeRresult[i].score < likeRresult[i-1].score) {
+          likeRresult[i].ranking = i+1;
         }
-        else
-          rankingInfo.voted = false;
+      }
+    }
+
+    // 비호감 순 줄 세우기
+    var unlikeRresult = [];
+    for (var i=0; i<unlikeRankingQuery.length; i++){
+      var rankingInfo = {};
+
+      rankingInfo.id = unlikeRankingQuery[i].id;
+      rankingInfo.score = unlikeRankingQuery[i].score;
       
-      result.push(rankingInfo);
+      unlikeRresult.push(rankingInfo);
+    }
+    // 비호감 순 랭킹 뽑기
+    for (var i=0; i<unlikeRresult.length; i++) {
+      if (i==0) {
+        unlikeRresult[i].ranking = 1;
+      } else {
+        if(unlikeRresult[i].score == unlikeRresult[i-1].score) {
+          unlikeRresult[i].ranking = unlikeRresult[i-1].ranking;
+        } else if (unlikeRresult[i].score < unlikeRresult[i-1].score) {
+          unlikeRresult[i].ranking = i+1;
+        }
+      }
+    }
+
+
+    // 해당 의원의 정보 가져오기 : 사진, 이름, 소속정당, 지역구 등 and 호감, 비호감 순위 및 득표수
+    var result = {};
+
+    for (var i=0; i<likeRresult.length; i++) {
+      if(likeRresult[i].id == l_id) {
+
+        result.l_id = likeRresult[i].id;
+        result.l_name = likeRankingQuery[i].name;
+        result.party_name = likeRankingQuery[i].l_party_name;
+
+        result.info = '';
+        if (likeRankingQuery[i].isPpresident) { // 당대표
+          result.info += "당 대표"
+        }
+        if (likeRankingQuery[i].isLpresident) { // 원내대표
+          result.info += "원내 대표"
+        }
+        if (likeRankingQuery[i].isPPpresident) { // 비례대표
+          if (result.info != '')
+            result.info += ", "
+          result.info += "비례 대표"
+        }
+        if (likeRankingQuery[i].region_city != "") {  // 지역구+선거구
+          if (result.info != '')
+            result.info += ", "
+          result.info += likeRankingQuery[i].region_city + " ";
+          result.info += likeRankingQuery[i].region_state;
+        }
+
+        result.profileimg = likeRankingQuery[i].profile_img_url;
+        result.like_ranking = likeRresult[i].ranking;
+        result.like_score = likeRresult[i].score;
+
+        break;
+      }
+    }
+
+    for (var i=0; i<unlikeRresult.length; i++) {
+      if(unlikeRresult[i].id == l_id) {
+
+        result.unlike_ranking = unlikeRresult[i].ranking;
+        result.unlike_score = unlikeRresult[i].score;
+
+        break;
+      }
     }
 
     res.status(200).send({
