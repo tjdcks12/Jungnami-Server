926071c478fc8c9c5dbd587f3676984d626be313
jiyeonkim7 <jiyean9629@gmail.com>
2018-07-04 15:19:52 +0900

    ranking/rankinglist modified _jiyeon

--- routes/ranking/rankinglist.js
+++ routes/ranking/rankinglist.js
@@ -6,48 +6,71 @@ const db = require('../../module/pool.js');
 
 /*  호감, 비호감 순 리스트  */
 /*  /ranking/list/:islike  */
-router.get('/:islike', async(req, res, next) => {
+router.get('/:islike/:u_id', async(req, res, next) => {
 
-  let islike = req.params.islike;
+  let islike =+ req.params.islike;
+  let u_id = req.params.u_id;
 
-  let selectSql = "SELECT * FROM legislator LEFT OUTER JOIN"
-      selectSql += "(SELECT lv_legislator_id, count(*) as score FROM legislatorVote"
-      selectSql += "WHERE islike is ? GROUP BY lv_legislator_id) as lv"
-      selectSql += "ON legislator.id = lv.lv_legislator_id ORDER BY lv.score DESC;"
+  let listSql = "SELECT * FROM legislator LEFT OUTER JOIN "
+      listSql += "(SELECT lv_legislator_id, count(*) as score FROM legislatorVote "
+      listSql += "WHERE islike=? GROUP BY lv_legislator_id) as lv "
+      listSql += "ON legislator.id = lv.lv_legislator_id ORDER BY lv.score DESC;"
 
-  let selectQuery = await db.queryParamCnt_Arr(selectSql,[islike]);
+  let listQuery = await db.queryParamCnt_Arr(listSql,[islike]);
 
-  if(selectQuery.length == 0){
+  if(listQuery.length == 0){
+    console.log("query not ok");
+  }else{
+    console.log("query ok");
+  }
+
+  let votedSql = "SELECT * FROM legislatorVote WHERE lv_user_id = ? AND islike = ?;"
+  let votedQuery = await db.queryParamCnt_Arr(votedSql, [u_id, islike]);
+
+  if(listQuery.length == 0){
     console.log("query not ok");
   }else{
     console.log("query ok");
   }
 
   var result = [];
-  for(var i=0; i<selectQuery.length; i++){
+  for(var i=0; i<listQuery.length; i++){
     var rankingInfo = {};
 
-    rankingInfo.name = selectQuery[i].name;
-    rankingInfo.party_name = selectQuery[i].l_party_name;
+    rankingInfo.l_id = listQuery[i].id;
+    rankingInfo.l_name = listQuery[i].name;
+    rankingInfo.party_name = listQuery[i].l_party_name;
 
-    rankingInfo.info = null;
-    if (selectQuery[i].isPpresident) { // 당대표
-      rankingInfo.info += "당 대표 "
+    rankingInfo.info = '';
+    if (listQuery[i].isPpresident) { // 당대표
+      rankingInfo.info += "당 대표"
     }
-    if (selectQuery[i].isLpresident) { // 원내대표
-      rankingInfo.info += "원내 대표 "
+    if (listQuery[i].isLpresident) { // 원내대표
+      rankingInfo.info += "원내 대표"
     }
-    if (selectQuery[i].isPPpresident) { // 비례대표
-      rankingInfo.info += "비례 대표 "
+    if (listQuery[i].isPPpresident) { // 비례대표
+      if (rankingInfo.info != '')
+        rankingInfo.info += ", "
+      rankingInfo.info += "비례 대표"
     }
-    if (selectQuery[i].region_city != "") {  // 지역구+선거구
-      rankingInfo.info += selectQuery[i].region_city + " ";
-      rankingInfo.info += selectQuery[i].region_state;
+    if (listQuery[i].region_city != "") {  // 지역구+선거구
+      if (rankingInfo.info != '')
+        rankingInfo.info += ", "
+      rankingInfo.info += listQuery[i].region_city + " ";
+      rankingInfo.info += listQuery[i].region_state;
     }
     
-    rankingInfo.score = selectQuery[i].score;
-    rankingInfo.profileimg = selectQuery[i].profile_img_url;
-    rankingInfo.mainimg = selectQuery[i].main_img_url;
+    rankingInfo.score = listQuery[i].score;
+    rankingInfo.profileimg = listQuery[i].profile_img_url;
+    rankingInfo.mainimg = listQuery[i].main_img_url;
+
+    for (var j=0; j<votedQuery.length; j++)
+      if (listQuery[i].id == votedQuery[j].lv_legislator_id) {
+        rankingInfo.voted = true;
+        break;
+      }
+      else
+        rankingInfo.voted = false;
     
     result.push(rankingInfo);
   }
