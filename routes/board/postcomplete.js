649d9b10a5fb23e21951b9a7efdaf90091b6eae2
sunnyangee <hwagae2003@gmail.com>
2018-07-05 18:12:15 +0900

    postcomplete 수정완료

--- routes/board/postcomplete.js
+++ routes/board/postcomplete.js
@@ -1,5 +1,4 @@
 //커뮤니티 글 작성 완료 버튼 눌렀을 때 작성 완료 - OK 
-//(board 테이블에 작성자 user_id unique 풀어줘야 함 )
 var express = require('express');
 var router = express.Router();
 const async = require('async');
@@ -8,7 +7,7 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
 
-aws.config.loadFromPath('../config/aws_config.json');
+aws.config.loadFromPath('./config/aws_config.json');
 
 const s3 = new aws.S3();
 
@@ -18,7 +17,7 @@ const upload = multer({
         bucket: 'myrubysbucket',
         acl: 'public-read',
         key: function(req, file, cb) {
-            cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+            cb(null, "board/" + file.originalname);
         }
     })
 });
@@ -30,8 +29,10 @@ router.post('/', upload.array('image'), async(req, res) => {
 				message : "please input b_user_id and content and shared"
 			});
 		}else{
-			let postboardQuery = 'INSERT INTO myjungnami.board(id, b_user_id, content, img_url, shared) VALUES (null, ?, ?, ?, ?)';
+
+			let postboardQuery = 'INSERT INTO myjungnami.board( b_user_id, content, img_url, shared) VALUES ( ?, ?, ?, ?)';
 			let data = await db.queryParamCnt_Arr(postboardQuery, [req.body.b_user_id, req.body.content, req.files[0].location, req.body.shared]);
+
 
 			res.status(200).send({
 				"message" : "insert posting success",
