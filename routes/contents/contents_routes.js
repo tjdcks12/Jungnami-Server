f5e5289d5be774527b55aa993c034456eabc078e
leejongchan <jongchanlee93@google.com>
2018-07-08 19:42:29 +0900

    contents route jongchan

--- routes/contents/contents_routes.js
+++ routes/contents/contents_routes.js
@@ -4,32 +4,35 @@ const router = express.Router();
 //1. 컨텐츠 탭 메인화면 - ok
 router.use('/main', require('./main'));
 
-//3. 컨텐츠 하나 클릭시에 카드뉴스 뿌려주기  - ok
+//2. 컨텐츠 하나 클릭시에 카드뉴스 뿌려주기  - ok
 router.use('/cardnews', require('./cardnews'));
 
-//4. 컨텐츠 좋아요
+//3. 컨텐츠 좋아요
 router.use('/like', require('./like'));
 
-//5. 컨텐츠에 달린 댓글리스트 보여주기
+//4. 컨텐츠에 달린 댓글리스트 보여주기
 router.use('/commentlist', require('./commentlist'));
 
-//6. 컨텐츠에 댓글 작성하기
+//5. 컨텐츠에 댓글 작성하기
 router.use('/makecomment', require('./makecomment'));
 
-//7. 컨텐츠에 댓글 좋아요
+//6. 컨텐츠에 댓글 좋아요
 router.use('/likecomment', require('./likecomment'));
 
-//8. 컨텐츠 대댓글 목록 보여주기
+//7. 컨텐츠 대댓글 목록 보여주기
 router.use('/recommentlist', require('./recommentlist'));
 
-//9. 컨텐츠에 대댓글 달기
+//8. 컨텐츠에 대댓글 달기
 router.use('/makerecomment', require('./makerecomment'));
 
-//10. 컨텐츠에 대댓글 좋아요
+//9. 컨텐츠에 대댓글 좋아요
 router.use('/likerecomment', require('./likerecomment'));
 
-//11. 컨텐츠 스크랩
+//10. 컨텐츠 스크랩
 router.use('/scrap', require('./scrap'));
+
+//11. 컨텐츠 작성
+router.use('/post', require('./post'));
 
 
 module.exports = router;
