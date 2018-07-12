10bf836eb11f33f9bf1dab9019c3bb8f4e3e78ea
jiyeonkim7 <jiyean9629@gmail.com>
2018-07-12 17:36:31 +0900

    modify unfollow _jiyeon

--- routes/user/unfollow.js
+++ routes/user/unfollow.js
@@ -10,7 +10,7 @@ const jwt = require('../../module/jwt.js');
 
 /*  내가 다른 유저를 언팔로우하기  */
 /*  /user/unfollow/  */
-router.post('/', async(req, res, next) => {
+router.delete('/:f_id', async(req, res, next) => {
 
   try {
 
@@ -24,12 +24,12 @@ router.post('/', async(req, res, next) => {
     }
 
     let follower_id = chkToken.id;
-    let following_id = req.body.following_id;
+    let following_id = req.params.f_id;
 
     let insertSql = "DELETE FROM follow WHERE f_follower_id = ? AND f_following_id = ?;"
     let insertQuery = await db.queryParamCnt_Arr(insertSql,[follower_id, following_id]);
 
-    res.status(201).send({
+    res.status(200).send({
       message : "Delete Data Success"
     });
 
