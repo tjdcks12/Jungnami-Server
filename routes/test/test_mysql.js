ccc79ef7df63509d506840291326261cdd95174a
jiyeonkim7 <jiyean9629@gmail.com>
2018-07-13 14:39:00 +0900

    test voting _jiyeon

--- routes/test/test_mysql.js
+++ routes/test/test_mysql.js
@@ -8,17 +8,37 @@ const async = require('async');
 const db = require('../../module/pool.js');
 
 /* database test */
-router.get('/', async(req, res, next) => {
+router.post('/', async(req, res, next) => {
   console.log("test");
 
 
+  let u_id = req.body.u_id;
+  let l_id =+ req.body.l_id;
+  let islike =+ req.body.islike;
+
+  for(var i=0; i<458; i++){
+    let insertSql = "INSERT INTO legislatorVote (lv_legislator_id, lv_user_id, islike) VALUES (?, ?, ?);"
+    let insertQuery = await db.queryParamCnt_Arr(insertSql,[l_id, u_id, islike]);
+
+    if(insertQuery == undefined){
+      res.status(204).send({
+        "message" : "insert error"
+      });
+
+      return;
+    }
+  }
+  res.status(201).send({
+        "message" : "successfully voting"
+  });
+
 //p_follower_id p_boardComment_id p_boardLike_id
 
-  let updatesql = "update user set img_url = '0' "
-      updatesql += "where img_url is null;"
-  let updatedate = await db.queryParamCnt_Arr(updatesql, []);
+  // let updatesql = "update user set img_url = '0' "
+  //     updatesql += "where img_url is null;"
+  // let updatedate = await db.queryParamCnt_Arr(updatesql, []);
 
-  console.log(updatedate.affectedRows)
+  // console.log(updatedate.affectedRows)
 
 
 
@@ -28,19 +48,19 @@ router.get('/', async(req, res, next) => {
   // console.log(deletedate.affectedRows)
 
 
-  if (updatedate.affectedRows < 1) {
+  // if (updatedate.affectedRows < 1) {
 
-    res.json({
-        message : "delete error",
-        status : 204
-      });
-  } else {
+  //   res.json({
+  //       message : "delete error",
+  //       status : 204
+  //     });
+  // } else {
 
-    res.json({
-        message : "delete success",
-        status : 200
-      });
-  }
+  //   res.json({
+  //       message : "delete success",
+  //       status : 200
+  //     });
+  // }
 
   // let testsql = "SELECT * FROM user";
   // let result = await db.queryParamCnt_Arr(testsql,[]);
