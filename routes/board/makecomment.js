bf1b4bab32ba3c98b151eb154e12459f93e86956
leejongchan <jongchanlee93@gmail.com>
2018-07-13 16:31:11 +0900

    modify push jongchan

--- routes/board/makecomment.js
+++ routes/board/makecomment.js
@@ -48,62 +48,63 @@ router.post('/', async(req, res) => {
 				return;
 			}
 
+			if(userid != result_find[0].b_user_id){
+				// push table에 insert
+				bc_id = data.insertId;
 
-			// push table에 insert
-			bc_id = data.insertId;
+				let pushSql = "INSERT INTO push (p_user_id, p_boardComment_id) VALUES (?, ?);"
+				let pushQuery = await db.queryParamCnt_Arr(pushSql,[result_find[0].b_user_id, bc_id]);
+				if(pushQuery == undefined){
+					res.status(204).send({
+						"message" : "fail insert"
+					});
 
-		    let pushSql = "INSERT INTO push (p_user_id, p_boardComment_id) VALUES (?, ?);"
-		    let pushQuery = await db.queryParamCnt_Arr(pushSql,[result_find[0].b_user_id, bc_id]);
-		    if(pushQuery == undefined){
-		      res.status(204).send({
-		        "message" : "fail insert"
-		      });
-
-		      return;
-		    }
+					return;
+				}
 
 
-			// 유저이름 가져오기
-			let select_user = 'SELECT * FROM user WHERE id = ?'
-			let result_user = await db.queryParamCnt_Arr(select_user, [userid] );
-			if(result_user.length == 0){
-				res.status(300).send({
-					message: "No Data"
-				});
-				return;
+				// 유저이름 가져오기
+				let select_user = 'SELECT * FROM user WHERE id = ?'
+				let result_user = await db.queryParamCnt_Arr(select_user, [userid] );
+				if(result_user.length == 0){
+					res.status(300).send({
+						message: "No Data"
+					});
+					return;
+				}
+
+				var pushmsg = (result_user[0].nickname + '님이 회원님의 글에 댓글을 남겼습니다.');
+				// client fcmToken 가져오기
+				let select_fcmtoken = 'SELECT fcmToken FROM user WHERE id = ?';
+				let result_fcmtoken = await db.queryParamCnt_Arr(select_fcmtoken, [result_find[0].b_user_id]);
+				if(result_fcmtoken.length == 0){
+					res.status(300).send({
+						message: "No Data"
+					});
+					return;
+				}
+
+				if(result_fcmtoken[0].fcmToken != null){
+					var push_data = await get_pushdata.get_pushdata(result_fcmtoken[0].fcmToken, pushmsg);
+					var fcm = new FCM(serverKey);
+
+					fcm.send(push_data, function(err, response) {
+						//console.log(push_data);
+						if (err) {
+							console.error('Push메시지 발송에 실패했습니다.');
+							console.error(err);
+							return;
+						}
+
+						console.log('Push메시지가 발송되었습니다.');
+						console.log(response);
+					});
+				}
+				else {
+					console.log("No fcmToken");
+				}
+				// 푸쉬알람 끝
 			}
-
-			var pushmsg = (result_user[0].nickname + '님이 회원님의 글에 댓글을 남겼습니다.');
-			// client fcmToken 가져오기
-			let select_fcmtoken = 'SELECT fcmToken FROM user WHERE id = ?';
-			let result_fcmtoken = await db.queryParamCnt_Arr(select_fcmtoken, [result_find[0].b_user_id]);
-			if(result_fcmtoken.length == 0){
-				res.status(300).send({
-					message: "No Data"
-				});
-				return;
-			}
-
-			if(result_fcmtoken[0].fcmToken != null){
-				var push_data = await get_pushdata.get_pushdata(result_fcmtoken[0].fcmToken, pushmsg);
-				var fcm = new FCM(serverKey);
-
-				fcm.send(push_data, function(err, response) {
-					//console.log(push_data);
-					if (err) {
-						console.error('Push메시지 발송에 실패했습니다.');
-						console.error(err);
-						return;
-					}
-
-					console.log('Push메시지가 발송되었습니다.');
-					console.log(response);
-				});
-			}
-			else {
-				console.log("No fcmToken");
-			}
-			// 푸쉬알람 끝
 		}
 		res.status(201).send({
 			"message" : "Successfully insert postmakecommentQuery"
